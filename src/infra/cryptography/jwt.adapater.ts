6c843460cf6eaf9de541b2e2d3708bdc3e3626ec
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-18 20:58:22 -0300

    feat: Jwt token must to be generate with expire date

--- src/infra/cryptography/jwt.adapater.ts
+++ src/infra/cryptography/jwt.adapater.ts
@@ -6,7 +6,11 @@ export class JwtAdapter implements Encrypter, Decrypter {
     constructor (private readonly secret: string) {}
 
     async encrypt (plaintext: string): Promise<string> {
-        return jwt.sign({ id: plaintext }, this.secret)
+        return jwt.sign(
+            { id: plaintext }, 
+            this.secret, 
+            { expiresIn: 60 * 60 * 24 * 30 }
+        );
     }
 
     async decrypt (ciphertext: string): Promise<string> {
