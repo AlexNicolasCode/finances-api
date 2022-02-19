da8853664d9d36edc8b63c9831369ee484736c43
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-19 16:36:30 -0300

    feat: AccountRepository should add account on db when execute add method

--- src/infra/db/account.repository.ts
+++ src/infra/db/account.repository.ts
@@ -1,8 +1,19 @@
 import { prisma } from ".";
 
 import { CheckAccountByEmailRepository, LoadAccountByEmailRepository } from "src/data/protocols";
+import { AddAccount } from "src/domain/usecases";
 
-export class AccountRepository implements LoadAccountByEmailRepository, CheckAccountByEmailRepository {
+export class AccountRepository implements LoadAccountByEmailRepository, CheckAccountByEmailRepository, AddAccount { 
+    async add ({ email, password }: AddAccount.Params): Promise<AddAccount.Result> {
+        const result = await prisma.user.create({
+            data: {
+                email,
+                password
+            }
+        }) 
+        return result ? true : false
+    }
+
     async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result> {
         const account = await prisma.user.findUnique({
             where: {
