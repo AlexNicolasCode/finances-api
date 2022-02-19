2c6f3308e888f3652ebbb87bda849798a40b3d31
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-18 21:00:14 -0300

    chore: useless functions must to be deleted

--- src/auth/encode-password/index.ts
+++
@@ -1,7 +0,0 @@
-import { createHmac } from "crypto";
-
-export const encondePassword = (password) => {
-    return createHmac("sha256", process.env.KEY_PASSWORD)
-      .update(password)
-      .digest("hex");
-}
--- src/auth/get-token/index.ts
+++
@@ -1,5 +0,0 @@
-import * as jwt from 'jsonwebtoken';
-
-export const getToken = (email) => jwt.sign({ email }, process.env.SECRET, {
-    expiresIn: 60 * 60 * 24 * 30
-});
