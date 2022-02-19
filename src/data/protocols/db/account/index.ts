55ed6304a199636e123dde6fc5a9d699c6b5ad78
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-19 16:27:33 -0300

    feat: DbAddAccount can add account when execute add method

--- src/data/protocols/db/account/index.ts
+++ src/data/protocols/db/account/index.ts
@@ -1,2 +1,3 @@
 export * from './load.account.by.email.repository'
 export * from './check.account.by.emai.repository'
+export * from './add.account.repository'
---
+++ src/data/usecases/db.add.account.ts
@@ -0,0 +1,20 @@
+import { AddAccount } from "src/domain/usecases";
+import { AddAccountRepository, CheckAccountByEmailRepository, Hasher } from "../protocols";
+
+export class DbAddAccount implements AddAccount {
+    constructor (
+        private readonly hasher: Hasher,
+        private readonly addAccountRepository: AddAccountRepository,
+        private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
+    ) {}
+
+    async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
+        const hasAccount = await this.checkAccountByEmailRepository.checkByEmail(accountData.password)
+        if (!hasAccount) {
+            const hashedPassword = await this.hasher.hash(accountData.password)
+            const isValid = await this.addAccountRepository.add({...accountData, password: hashedPassword })
+            return isValid
+        }
+        return false
+    }
+}
