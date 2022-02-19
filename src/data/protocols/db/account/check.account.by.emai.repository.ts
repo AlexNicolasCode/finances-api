0d02ebb04c3805c0c0e9dcf7ae2c8037935ae7ef
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-19 16:07:02 -0300

    feat: CheckAccountByEmailRepostiry was defined

---
+++ src/data/protocols/db/account/check.account.by.emai.repository.ts
@@ -0,0 +1,7 @@
+export interface CheckAccountByEmailRepostiry {
+    checkByEmail: (email: string) => Promise<CheckAccountByEmailRepository.Result>
+}
+
+export namespace CheckAccountByEmailRepository {
+    export type Result = boolean
+}
