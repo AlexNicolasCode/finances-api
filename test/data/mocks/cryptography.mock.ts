31a91791611d5fb07f0b2d5696c7ab9cc49a772e
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-25 10:41:19 -0300

    feat: HashComparerSpy has mocked

--- test/data/mocks/cryptography.mock.ts
+++ test/data/mocks/cryptography.mock.ts
@@ -1,6 +1,6 @@
 import faker from '@faker-js/faker'
 
-import { Hasher } from 'src/data/protocols'
+import { HashComparer, Hasher } from 'src/data/protocols'
 
 export class HasherSpy implements Hasher {
     digest = faker.datatype.uuid()
@@ -9,5 +9,17 @@ export class HasherSpy implements Hasher {
     async hash (plaintext: string): Promise<string> {
         this.plaintext = plaintext
         return this.digest
+    }
+}
+
+export class HashComparerSpy implements HashComparer {
+    digest: string
+    plaintext: string
+    isValid = true
+
+    async compare (plaintext: string, digest: string): Promise<boolean> {
+        this.plaintext = plaintext
+        this.digest = digest
+        return this.isValid
     }
 }
