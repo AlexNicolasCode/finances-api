ba2f36f2abcac573ceee6101cf7305080386576d
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-25 20:18:11 -0300

    test: JwtAdapter should return accessToken on success

--- test/infra/cryptgraphy/jwt.adapter.spec.ts
+++ test/infra/cryptgraphy/jwt.adapter.spec.ts
@@ -5,23 +5,35 @@ import { JwtAdapter } from 'src/infra/cryptography'
 
 jest.mock('jsonwebtoken', () => ({
     async sign (): Promise<string> {
-        return faker.datatype.string(5)
+        return 'any_token'
     },
 
     async verify (): Promise<string> {
-        return faker.datatype.string(5)
+        return 'any_value'
     }
 }))
+
+const makeSut = (): JwtAdapter => {
+    return new JwtAdapter('secret')
+} 
 
 describe('JwtAdapter', () => {
     describe('sign()', () => {
         test('Should call jwt.sign with params', async () => {
-            const sut = new JwtAdapter('secret')
+            const sut = makeSut()
             const signSpy = jest.spyOn(jwt, 'sign')
 
             await sut.encrypt('any_id')
 
             expect(signSpy).toHaveBeenCalledWith({ id: 'any_id'}, 'secret', { expiresIn: 60 * 60 * 24 * 30 })
+        })
+
+        test('Should return accessToken on success', async () => {
+            const sut = makeSut()
+
+            const accessToken = await sut.encrypt('any_id')
+
+            expect(accessToken).toBe('any_token')
         })
     })
 })
