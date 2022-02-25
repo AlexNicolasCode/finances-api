50a0a73470182377e653bb0a4597d3ea9409c7cb
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-24 23:31:33 -0300

    feat: PrismaAdapter is used on AccountAccountRepository

--- src/main/factories/usecases/authentication.factory.ts
+++ src/main/factories/usecases/authentication.factory.ts
@@ -1,13 +1,15 @@
 import env from "src/main/config/env";
 import { Authentication } from "src/domain/usecases";
 import { BycryptApapter, JwtAdapter } from "src/infra/cryptography";
-import { AccountRepository } from "src/infra/db/account.repository";
-import { DbAuthentication } from "src/data/usecases/db.authentication";
+import { AccountRepository } from "src/infra/db";
+import { DbAuthentication } from "src/data/usecases";
+import { PrismaAdapter } from "src/infra/db";
 
 export const makeDbAuthentication = (): Authentication => {
     const salt = 12
     const bcryptAdapter = new BycryptApapter(salt)
     const jwtAdapter = new JwtAdapter(env.jwtSecret)
-    const accountRepository = new AccountRepository()
+    const prismaAdapter = new PrismaAdapter()
+    const accountRepository = new AccountRepository(prismaAdapter, prismaAdapter)
     return new DbAuthentication(accountRepository, bcryptAdapter, jwtAdapter)
 } 
