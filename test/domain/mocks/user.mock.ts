f79bb2b7f0f3d2ff38374c857b81d7e61b39a0d0
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-25 12:53:11 -0300

    feat: UserModel mock must include id

--- test/domain/mocks/user.mock.ts
+++ test/domain/mocks/user.mock.ts
@@ -3,6 +3,7 @@ import { UserModel } from "src/domain/models";
 import { faker } from '@faker-js/faker'
 
 export const mockUserModel = (): UserModel => ({
+    id: faker.datatype.uuid(),
     email: faker.internet.email(),
     password: faker.internet.password()
 })
