cf2af66436ef81c4674b7ffc7a00f34ca3b93879
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-25 20:45:22 -0300

    refactor: change BycryptApapter to BcryptAdapter name

--- src/infra/cryptography/bcrypt.adapter.ts
+++ src/infra/cryptography/bcrypt.adapter.ts
@@ -2,7 +2,7 @@ import * as bcrypt from 'bcrypt';
 
 import { HashComparer, Hasher } from "src/data/protocols";
 
-export class BycryptApapter implements Hasher, HashComparer {
+export class BcryptAdapter implements Hasher, HashComparer {
     constructor (private readonly salt: number) {}
 
     async hash (plaintext: string): Promise<string> {
