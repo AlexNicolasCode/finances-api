4b7e7b75448d41db2abb656b9c60f29ec99b9cbc
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-25 12:33:09 -0300

    fix: mockUserModel must return valid email and password

--- test/domain/mocks/user.mock.ts
+++ test/domain/mocks/user.mock.ts
@@ -3,6 +3,6 @@ import { UserModel } from "src/domain/models";
 import { faker } from '@faker-js/faker'
 
 export const mockUserModel = (): UserModel => ({
-    email: faker.lorem.words(),
-    password: faker.lorem.words()
+    email: faker.internet.email(),
+    password: faker.internet.password()
 })
