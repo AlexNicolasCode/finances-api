18d928a2deaa060bee262224666870c662ce0284
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-26 18:36:45 -0300

    test: AccountBalanceRepository should throw if loadAccountBalanceById throws

--- test/infra/db/account.balance.repository.spec.ts
+++ test/infra/db/account.balance.repository.spec.ts
@@ -1,16 +1,47 @@
 import { AccountBalanceRepository } from "src/infra/db"
 import { LoadAccountBalanceByIdRepositorySpy } from "test/data/mocks"
-import { mockUserModel } from "test/domain/mocks"
+import { mockUserModel, throwError } from "test/domain/mocks"
 
+type SutType = {
+    loadAccountBalanceById: LoadAccountBalanceByIdRepositorySpy
+    userIdMock: number
+    sut: AccountBalanceRepository
+}
+
+const makeSut = (): SutType => {
+    const loadAccountBalanceById = new LoadAccountBalanceByIdRepositorySpy()
+    const userIdMock = mockUserModel().id
+    const sut = new AccountBalanceRepository(loadAccountBalanceById)
+    return {
+        sut,
+        loadAccountBalanceById,
+        userIdMock
+    }
+}
 
 describe('AccountBalanceRepository', () => {
     test('Should return AccountBalance on success', async () => {
-        const loadAccountBalanceById = new LoadAccountBalanceByIdRepositorySpy()
-        const userIdMock = mockUserModel().id
-        const sut = new AccountBalanceRepository(loadAccountBalanceById)
+        const {
+            sut,
+            loadAccountBalanceById,
+            userIdMock
+        } = makeSut()
 
         const result = await sut.loadAccountBalanceById(userIdMock)
 
         expect(result).toBe(loadAccountBalanceById.result)
+    })
+
+    test('Should throw if loadAccountBalanceById throws', async () => {
+        const {
+            sut,
+            loadAccountBalanceById,
+            userIdMock
+        } = makeSut()
+        jest.spyOn(loadAccountBalanceById, 'loadAccountBalanceById').mockImplementationOnce(throwError)
+
+        const promise = sut.loadAccountBalanceById(userIdMock)
+
+        expect(promise).rejects.toThrow()
     })
 })
