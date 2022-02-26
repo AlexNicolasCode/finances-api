ec7cdfac1d42a5f6e8a4ea8d33dc07938e07680f
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-26 16:25:38 -0300

    feat: UserModel mock mustn't include accessToken field

--- test/domain/mocks/user.mock.ts
+++ test/domain/mocks/user.mock.ts
@@ -1,11 +1,9 @@
 import { faker } from '@faker-js/faker'
 
 import { UserModel } from "src/domain/models"
-import { mockAccessToken } from "."
 
 export const mockUserModel = (): UserModel => ({
     id: faker.datatype.number(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-    accessToken: mockAccessToken().accessToken
 })
