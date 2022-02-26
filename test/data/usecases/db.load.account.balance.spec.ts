0805e31de92b87e542a9f43d42564f9fb2ff80fa
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-26 02:09:39 -0300

    test: DbLoadAccountBalance should return undefined if loadAccountByAccessTokenRepository not found an account

---
+++ test/data/usecases/db.load.account.balance.spec.ts
@@ -0,0 +1,30 @@
+import { LoadAccountBalance } from "src/domain/usecases"
+import { LoadAccountBalanceByEmail, LoadAccountByAccessTokenRepository } from "src/data/protocols"
+import { LoadAccountBalanceByEmailRepositorySpy, LoadAccountByAccessTokenRepositorySpy } from "../mocks"
+
+class DbLoadAccountBalance implements LoadAccountBalance {
+    constructor (
+        private readonly loadAccountByAccessTokenRepository: LoadAccountByAccessTokenRepository,
+        private readonly loadAccountBalanceByEmailRepositorySpy: LoadAccountBalanceByEmail
+    ) {}
+
+    async load ({ accessToken }: LoadAccountBalance.Params): Promise<LoadAccountBalance.Result> {
+        const account = await this.loadAccountByAccessTokenRepository.loadByAccessToken(accessToken)
+        if (!account) return
+        return this.loadAccountBalanceByEmailRepositorySpy.loadByEmail(account.email)
+    }
+}
+
+
+describe('DbLoadAccountBalance', () => {
+    test('Should return undefined if loadAccountByAccessTokenRepository not found an account', async () => {
+        const loadAccountByAccessTokenRepositorySpy = new LoadAccountByAccessTokenRepositorySpy()
+        jest.spyOn(loadAccountByAccessTokenRepositorySpy, 'loadByAccessToken').mockImplementationOnce(() => undefined)
+        const loadAccountBalanceByEmailRepositorySpy = new LoadAccountBalanceByEmailRepositorySpy()
+        const sut = new DbLoadAccountBalance(loadAccountByAccessTokenRepositorySpy, loadAccountBalanceByEmailRepositorySpy)
+
+        const result = await sut.load({ accessToken: 'any_token' })
+
+        expect(result).toBeUndefined()
+    })
+})
--- test/domain/mocks/index.ts
+++ test/domain/mocks/index.ts
@@ -1,3 +1,4 @@
 export * from './user.mock'
 export * from './test.helpers'
 export * from './access.token'
+export * from './account.balance'
