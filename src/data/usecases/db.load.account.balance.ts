9398c4e04b91a41ced056133b0af5add9b5d489f
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-26 18:02:57 -0300

    feat: DbLoadAccountBalance must find account balance using user id

--- src/data/usecases/db.load.account.balance.ts
+++ src/data/usecases/db.load.account.balance.ts
@@ -1,15 +1,18 @@
 import { LoadAccountBalance } from "src/domain/usecases"
-import { Decrypter, LoadAccountBalanceByEmail } from "../protocols"
+import { Decrypter, LoadAccountBalanceById, LoadAccountByEmailRepository } from "../protocols"
 
 export class DbLoadAccountBalance implements LoadAccountBalance {
     constructor (
         private readonly decrypter: Decrypter,
-        private readonly loadAccountBalanceByEmailRepository: LoadAccountBalanceByEmail
+        private readonly loadAccountByEmail: LoadAccountByEmailRepository,
+        private readonly loadAccountBalanceByIdRepository: LoadAccountBalanceById,
     ) {}
 
     async load ({ accessToken }: LoadAccountBalance.Params): Promise<LoadAccountBalance.Result> {
         const decryptedToken = await this.decrypter.decrypt(accessToken)
         if (!decryptedToken) return
-        return this.loadAccountBalanceByEmailRepository.loadByEmail(decryptedToken.email)
+        const account = await this.loadAccountByEmail.loadByEmail(decryptedToken.email)
+        if (!account) return
+        return await this.loadAccountBalanceByIdRepository.loadAccountBalanceById(account.id)
     }
 }
