6962ab8cc5b2665f1c2380fab5840ed83fcb40c1
Alex Nicolas <alexoliveira604@gmail.com>
2022-02-26 16:43:48 -0300

    refactor: loadAccountByAccessTokenRepository was changed to decrypter

--- src/data/usecases/db.load.account.balance.ts
+++ src/data/usecases/db.load.account.balance.ts
@@ -1,15 +1,15 @@
 import { LoadAccountBalance } from "src/domain/usecases"
-import { LoadAccountBalanceByEmail, LoadAccountByAccessTokenRepository } from "../protocols"
+import { Decrypter, LoadAccountBalanceByEmail } from "../protocols"
 
 export class DbLoadAccountBalance implements LoadAccountBalance {
     constructor (
-        private readonly loadAccountByAccessTokenRepository: LoadAccountByAccessTokenRepository,
-        private readonly loadAccountBalanceByEmailRepositorySpy: LoadAccountBalanceByEmail
+        private readonly decrypter: Decrypter,
+        private readonly loadAccountBalanceByEmailRepository: LoadAccountBalanceByEmail
     ) {}
 
     async load ({ accessToken }: LoadAccountBalance.Params): Promise<LoadAccountBalance.Result> {
-        const account = await this.loadAccountByAccessTokenRepository.loadByAccessToken(accessToken)
-        if (!account) return
-        return this.loadAccountBalanceByEmailRepositorySpy.loadByEmail(account.email)
+        const decryptedToken = await this.decrypter.decrypt(accessToken)
+        if (!decryptedToken) return
+        return this.loadAccountBalanceByEmailRepository.loadByEmail(decryptedToken.email)
     }
 }
